09e289fa8e5def6ebf950b37e6a7a2deb97892fa
imad.berkati <berkati.imad@gmail.com>
2019-10-13 19:51:12 +0200

    Add home and room-game components + Config rooter, bootstrap and sass

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { GameRoomComponent } from './game-room/game-room.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    GameRoomComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {
+        path:'',
+        component:HomeComponent
+      },
+      {
+        path:'game-room',
+        component:GameRoomComponent
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
