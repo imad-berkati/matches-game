0503df09092924c45ff2b8bb0201f078f1c0c904
imad.berkati <berkati.imad@gmail.com>
2019-10-15 09:07:34 +0200

    code formatting

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,33 +1,29 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { FormsModule } from '@angular/forms';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { RouterModule } from "@angular/router";
+import { FormsModule } from "@angular/forms";
 
-import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { GameRoomComponent } from './game-room/game-room.component';
+import { AppComponent } from "./app.component";
+import { HomeComponent } from "./home/home.component";
+import { GameRoomComponent } from "./game-room/game-room.component";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-    GameRoomComponent
-  ],
+  declarations: [AppComponent, HomeComponent, GameRoomComponent],
   imports: [
     BrowserModule,
     FormsModule,
     RouterModule.forRoot([
       {
-        path:'',
-        component:HomeComponent
+        path: "",
+        component: HomeComponent
       },
       {
-        path:'game-room',
-        component:GameRoomComponent
+        path: "game-room",
+        component: GameRoomComponent
       }
     ])
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,29 +1,28 @@
-import { Component, OnInit } from '@angular/core';
-import {Router} from "@angular/router"
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.scss"]
 })
 export class HomeComponent implements OnInit {
-
   /**
    * The player name.
-  */
+   */
   playerName: string;
 
-  constructor(private router:Router){ }
+  constructor(private router: Router) {}
 
-  ngOnInit() {
-
-  }
+  ngOnInit() {}
 
   /**
    * Submit function
    * Redirect and pass the player name to the 'game-room' page
    */
   submit() {
-    this.router.navigate(['/game-room'], { queryParams: { playerName: this.playerName }});
+    this.router.navigate(["/game-room"], {
+      queryParams: { playerName: this.playerName }
+    });
   }
 }
